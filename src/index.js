c1e2a627e063d16493c16bd603cd9e19bdebe8f2
Saúl Laínez <saullainez@hotmail.es>
2021-04-26 09:21:13 -0600

    useEffect
    SimpleForm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SimpleForm } from './components/useEffect/SimpleForm';
 //import { CounterApp } from './components/useState/CounterApp';
-import { CounterWithCustomHook } from './components/useState/CounterWithCustomHook';
+//import { CounterWithCustomHook } from './components/useState/CounterWithCustomHook';
 
 ReactDOM.render(
-  <CounterWithCustomHook />,
+  <SimpleForm />,
   document.getElementById('root')
 );
