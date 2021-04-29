a3575385aa8c6d5c6681326d87650d0a071ecd3b
Saúl Laínez <saullainez@hotmail.es>
2021-04-29 10:20:46 -0600

    createContext y useContext

--- src/components/useContext/MainApp.js
+++ src/components/useContext/MainApp.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const user = {
+        id:'123',
+        name:'Saul',
+        email:'saullainez@hotmail.es'
+    }
+
     return (
 
-        <AppRouter />
+        <UserContext.Provider value={user}>
+            <AppRouter />
+        </UserContext.Provider>
+        
 
     )
 }
