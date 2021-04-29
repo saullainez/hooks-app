0c936d95291592a08d887c015f243f5b5800d41b
Saúl Laínez <saullainez@hotmail.es>
2021-04-29 08:25:03 -0600

    Configurar router

--- src/components/useContext/MainApp.js
+++ src/components/useContext/MainApp.js
@@ -1,12 +1,10 @@
 import React from 'react'
+import { AppRouter } from './AppRouter'
 
 export const MainApp = () => {
     return (
-        <>
-            
-            <h1>MainApp</h1>
-            <hr />
 
-        </>
+        <AppRouter />
+
     )
 }
