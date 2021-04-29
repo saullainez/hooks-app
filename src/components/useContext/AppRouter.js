b7d0c93cccf75e88f1cacf74b29a7f5faba7569e
Saúl Laínez <saullainez@hotmail.es>
2021-04-29 09:44:47 -0600

    Link y Navlink

--- src/components/useContext/AppRouter.js
+++ src/components/useContext/AppRouter.js
@@ -7,16 +7,22 @@ import {
 import { AboutScreen } from './AboutScreen';
 import { HomeScreen } from './HomeScreen';
 import { LoginScreen } from './LoginScreen';
+import { NavBar } from './NavBar';
+import { NotFound } from './NotFound';
 
 export const AppRouter = () => {
     return (
 
         <Router>
             <div>
+
+                <NavBar/>
+
                 <Switch>
                     <Route exact path="/about" component={ AboutScreen }/>
                     <Route exact path="/login" component={ LoginScreen }/>
                     <Route exact path="/" component={ HomeScreen }/>
+                    <Route component={ NotFound }/>
                 </Switch>
             </div>
         </Router>
---
+++ src/components/useContext/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotFound = () => {
+    return (
+        <>
+            <h1>Página no encontrada</h1>
+        </>
+    )
+}
